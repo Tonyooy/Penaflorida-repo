3a3a77630cedada1cd2830f4a03c552d43ca9d88
Tony <115552736+Tonyooy@users.noreply.github.com>
2023-10-24 12:11:02 +0100

    Add files via upload

---
+++ PRELIM/SongLyrics.java
@@ -0,0 +1,11 @@
+public class SongLyrics{
+   public static void main(String [] args){
+      System.out.println("Song title: ");
+      System.out.println("Sunday Morning");
+      System.out.println("by Maroon 5");
+      System.out.println("\nSunday morning, rain is falling");
+      System.out.println("Steal some covers, share some skin (I like that)");
+      System.out.println("Clouds are shrouding us in moments unforgettable");
+      System.out.println("You twist to fit the mold that I am in");
+   }
+}
